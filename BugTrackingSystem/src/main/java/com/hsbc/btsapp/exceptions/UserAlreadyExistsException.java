0449a63669693e65419f41d85edf2eb6f2396657
NikitaAgarwal <nikitaagarwal0408@gmail.com>
2021-09-19 19:05:49 +0530

    Create UserAlreadyExistsException.java

---
+++ BugTrackingSystem/src/main/java/com/hsbc/btsapp/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.hsbc.btsapp.exceptions;
+
+public class UserAlreadyExistsException extends Exception {
+
+	public UserAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}
