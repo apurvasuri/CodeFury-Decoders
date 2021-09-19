f2524740deda311c20de1e1755bda1ba65f1535a
Smriti Rout <smriti.rout@gmail.com>
2021-09-19 23:03:46 +0530

    Update Project.java

--- BugTrackingSystem/src/main/java/com/hsbc/btsapp/beans/Project.java
+++ BugTrackingSystem/src/main/java/com/hsbc/btsapp/beans/Project.java
@@ -12,5 +12,65 @@ public class Project {
 	private Date projStartDate;
 	private Status projectStatus;
 	private int teamID;
+	
+	public Project() {
+		
+	}
+	
+	public Project(int projectId, String projectName, String projectDescription, Date projectStartDate,
+			Status projectStatus, int teamID) {
+		super();
+		this.projectId = projectId;
+		this.projectName = projectName;
+		this.projectDescription = projectDescription;
+		this.projectStartDate = projectStartDate;
+		this.projectStatus = projectStatus;
+		this.teamID = teamID;
+	}
+	
+	public int getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(int projectId) {
+		this.projectId = projectId;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getProjectDescription() {
+		return projectDescription;
+	}
+	public void setProjectDescription(String projectDescription) {
+		this.projectDescription = projectDescription;
+	}
+	public Date getProjectStartDate() {
+		return projectStartDate;
+	}
+	public void setProjStartDate(Date projectStartDate) {
+		this.projectStartDate = projectStartDate;
+	}
+	public Status getProjectStatus() {
+		return projectStatus;
+	}
+	public void setProjectStatus(Status projectStatus) {
+		this.projectStatus = projectStatus;
+	}
+	public int getTeamID() {
+		return teamID;
+	}
+	public void setTeamID(int teamID) {
+		this.teamID = teamID;
+	}
+	
+	@Override
+	public String toString() {
+		return "Project [projectId=" + projectId + ", projectName=" + projectName + ", projectDescription="
+				+ projectDescription + ", projStartDate=" + projectStartDate + ", projectStatus=" + projectStatus
+				+ ", teamID=" + teamID + "]";
+	}
+
 
 }
