e83c9827bfaeae5d13395d06422eeb5da76e5aaa
NikitaAgarwal <nikitaagarwal0408@gmail.com>
2021-09-19 19:05:45 +0530

    Update UserDAO.java

--- BugTrackingSystem/src/main/java/com/hsbc/btsapp/daos/interfaces/UserDAO.java
+++ BugTrackingSystem/src/main/java/com/hsbc/btsapp/daos/interfaces/UserDAO.java
@@ -3,15 +3,13 @@ package com.hsbc.btsapp.daos.interfaces;
 import java.util.List;
 
 import com.hsbc.btsapp.beans.User;
+import com.hsbc.btsapp.exceptions.UserAlreadyExistsException;
 
 public interface UserDAO {
-	
-	void addUser(User user);
+	void addUser(User user) throws UserAlreadyExistsException;
 	void updateUser(User user);
 	User getUserById(int userId);
 	User getUserByName(String userName);
 	List<User> getAllUsers();
 	void deleteUser(User user);
-
-	
 }
