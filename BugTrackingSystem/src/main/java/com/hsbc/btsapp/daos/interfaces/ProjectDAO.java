1c48e07c559c170a5812286a6f8d4edf526b1665
Smriti Rout <smriti.rout@gmail.com>
2021-09-19 23:15:02 +0530

    Update ProjectDAO.java

--- BugTrackingSystem/src/main/java/com/hsbc/btsapp/daos/interfaces/ProjectDAO.java
+++ BugTrackingSystem/src/main/java/com/hsbc/btsapp/daos/interfaces/ProjectDAO.java
@@ -7,10 +7,10 @@ import com.hsbc.btsapp.exceptions.ProjectAlreadyExistsException;
 public interface ProjectDao {
 	public void addProject(Project project) throws ProjectAlreadyExistsException;
 	public void updateProject(Project project);
-	public Project getProjectById(int projectId);
+	public Project getProjectById(int projectId) throws ProjectDoesNotExistException;
 	public Project getProjectByName(String projectName);
 	public List<Project> getAllProjects();
-	public void deleteProject(Project project);
+	public void deleteProject(Project project) throws ProjectDoesNotExistException;
 	
 
 }
